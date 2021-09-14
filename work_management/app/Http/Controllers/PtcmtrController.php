4278e54d2cfa84c4da7a0a73972a29f8c22209a1
unknown <hioki@b-forme.net>
2021-09-14 11:10:30 +0900

    'ログ出力サンプル追加'

--- work_management/app/Http/Controllers/PtcmtrController.php
+++ work_management/app/Http/Controllers/PtcmtrController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Librarys\php\TreeData;
 use Illuminate\Support\Facades\View;
+use App\Librarys\php\OutputLog;
 
 class PtcmtrController extends Controller
 {
@@ -11,5 +12,7 @@ class PtcmtrController extends Controller
         $tree_data = TreeData::generate_tree_data();
         View::share('tree_chain', $tree_data[0]);
         View::share('projection_chain', $tree_data[1]);
+        // OutputLog::log(__FUNCTION__, 'si', '共通機能', 'ツリーデータを生成');
+        // OutputLog::message_log(explode(DIRECTORY_SEPARATOR, __CLASS__)[count(explode(DIRECTORY_SEPARATOR, __CLASS__)) - 1].'/'.__FUNCTION__, 'mhcmok0009');
     }
 }

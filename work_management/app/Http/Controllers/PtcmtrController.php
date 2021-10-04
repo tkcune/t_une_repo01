dcacca129f2ce40e798142c9793cfae275dce06f
unknown <hioki@b-forme.net>
2021-10-04 14:34:31 +0900

    'ツリーの変更データをblade側にセットする'

--- work_management/app/Http/Controllers/PtcmtrController.php
+++ work_management/app/Http/Controllers/PtcmtrController.php
@@ -4,15 +4,33 @@ namespace App\Http\Controllers;
 
 use App\Librarys\php\TreeData;
 use Illuminate\Support\Facades\View;
-use App\Librarys\php\OutputLog;
 
 class PtcmtrController extends Controller
 {
+    //ツリーのデータをblade側にセットする
     static function set_view_treedata(){
         $tree_data = TreeData::generate_tree_data();
         View::share('tree_chain', $tree_data[0]);
         View::share('projection_chain', $tree_data[1]);
-        // OutputLog::log(__FUNCTION__, 'si', '共通機能', 'ツリーデータを生成');
-        // OutputLog::message_log(explode(DIRECTORY_SEPARATOR, __CLASS__)[count(explode(DIRECTORY_SEPARATOR, __CLASS__)) - 1].'/'.__FUNCTION__, 'mhcmok0009');
+    }
+
+    //ツリーの追加情報をblade側にセットする
+    static function append_node($id){
+        View::share('treeaction_chain', array('action' => 'append', 'id' => $id));
+    }
+
+    //ツリーの削除情報をblade側にセットする
+    static function delete_node($id){
+        View::share('treeaction_chain', array('action' => 'delete', 'id' => $id));
+    }
+
+    //ツリーの展開情報をblade側にセットする
+    static function open_node($id){
+        View::share('treeaction_chain', array('action' => 'open', 'id' => $id));
+    }
+
+    //ツリーの更新情報をblade側にセットする
+    static function update_node($id){
+        view::share('treeaction_chain', array('action' => 'update', 'id' => $id));
     }
 }

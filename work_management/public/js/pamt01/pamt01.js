35ba7385ffe0a600c07ef6b4403a403bbb9e8ecd
r-shimoda <r_shimoda@b-forme.net>
2021-10-13 13:59:11 +0900

    コメントの修正

--- work_management/public/js/pamt01/pamt01.js
+++ work_management/public/js/pamt01/pamt01.js
@@ -1,21 +1,21 @@
+/**
+ * 画面の隠蔽表示メソッド
+ * @param string div 対象のdiv
+ * @param string state 対象のdivの状態
+ */
+
 var display=function() {
     //切り替える対象の状態を取得
     var div = document.getElementById('tree');
     //取得した情報からスタイルについての状態のみをstateに代入
     state=div.style.display;
-    //デバッグ用にlogに出力
-    console.log(state);
     //非表示中のときの処理
     if(state=="none"){
          //スタイルを表示(inline)に切り替え
          div.setAttribute("style","display:inline");
-        //デバッグ用にinlineをlogに出力
-        console.log("inline");
     }else{
          //スタイルを非表示(none)に切り替え
         div.setAttribute("style","display:none");
-        //デバッグ用にnoneをlogに出力
-        console.log("none");
     }
 }
 
@@ -61,6 +61,12 @@ function passwordOn()
 	}
 }
 
+/**
+ * ツリーの隠蔽表示メソッド
+ * @param string id 対象のid
+ * @param string state 対象のdivの状態
+ */
+
 function treeDisabled()
 {
     var id = document.getElementById("tree");
@@ -78,6 +84,8 @@ function treeDisabled()
 
 /**
  * 複写した部署のIDを取得し各種フォームのvalueに当てはめるメソッド
+ * @param string id 対象のID
+ * 
  */
 function clickSave()
 {
@@ -89,6 +97,12 @@ function clickSave()
   document.getElementById("ji_lower_move").defaultValue = id;
 
 }
+
+/**
+ * 複写した部署を取り消すメソッド
+ * @param string id IDを空にするための引数
+ * 
+ */
 
 function clickDelete()
 {

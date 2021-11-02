3a844a02b4f80d37634732ebf820de0331b0f902
unknown <hioki@b-forme.net>
2021-11-02 10:36:21 +0900

    '削除や登録などの操作でのカレント情報をツリー画面に与える修正'

--- work_management/app/Http/Controllers/PtcmtrController.php
+++ work_management/app/Http/Controllers/PtcmtrController.php
@@ -9,24 +9,49 @@ use Illuminate\Support\Facades\View;
 class PtcmtrController extends Controller
 {
     //ツリーのデータをblade側にセットする
-    static function set_view_treedata(){
+    public static function set_view_treedata(){
         //@var array ツリーのデータを生成する
         $tree_data = TreeData::generate_tree_data();
         //ツリー生成データをセットする
         View::share('tree_chain', $tree_data[0]);
         //投影データをセットする
         View::share('projection_chain', $tree_data[1]);
+
+        //セッション情報のrecent_treeactionがnullの場合は、初期設定
+        if(session()->get('recent_treeaction', null) != null){
+            //削除または、登録のメソッドを通過したか、判断する
+            if(session()->get('recent_treeaction') == 'delete' || session()->get('recent_treeaction') == 'open'){
+                //前回のツリーアクション(back_treeaction)をdeleteか、openとする
+                session(['back_treeaction' => session()->get('recent_treeaction')]);
+                session(['recent_treeaction' => 'show']);
+            }else{
+                //削除や登録メソッドではない場合、show(表示メソッド)とする
+                session(['back_treeaction' => 'show']);
+                session(['recent_treeaction' => 'show']);
+                //表示メソッドなので、idを消去する
+                session()->forget('action_node_id');
+            }
+        }else{
+            //初期設定
+            session(['recent_treeaction' => 'show']);
+            session()->forget('back_treeaction');
+            session()->forget('action_node_id');
+        }
     }
 
     //ツリーの削除情報をblade側にセットする
     //@param string $id 削除したid
-    static function delete_node($id){
-        View::share('treeaction_chain', array('action' => 'delete', 'id' => $id));
+    public static function delete_node($id){
+        //親のidとアクション情報をセッション情報にセットする
+        session(['action_node_id'=>$id]);
+        session(['recent_treeaction' => 'delete']);
     }
 
     //ツリーの展開情報をblade側にセットする
     //@param string $id 追加、表示するid
-    static function open_node($id){
-        View::share('treeaction_chain', array('action' => 'open', 'id' => $id));
+    public static function open_node($id){
+        //登録先などのidとアクション情報をセッション情報にセットする
+        session(['action_node_id' => $id]);
+        session(['recent_treeaction' => 'open']);
     }
 }

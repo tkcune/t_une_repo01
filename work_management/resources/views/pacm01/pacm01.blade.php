5b089cdd084350b8a4db34dc10a3711bfff46e39
r-shimoda <r_shimoda@b-forme.net>
2021-12-03 11:16:00 +0900

    管理者検索の最大値設定と管理者リンクの設定

--- work_management/resources/views/pacm01/pacm01.blade.php
+++ work_management/resources/views/pacm01/pacm01.blade.php
@@ -34,12 +34,12 @@
                         value="{{$top_department[0]->management_personnel_id}}" style="width:100px;"></p>
                     </div>
                     <div class="col-3" style="padding:0px">
-                        <p>管理者名：{{$top_management[0]}}</p>
+                        <p>管理者名：<a href="{{ route('plbs01.show',[session('client_id'),$top_department[0]->management_personnel_id])}}">{{$top_management[0]}}</a></p>
                     </div>
                     <div class="col" style="padding:0px">
 
                     <p>管理者検索：
-                        <input type="text" id="search-list" list="keywords" style="width:150px;"autocomplete="on"
+                        <input type="text" id="search-list" list="keywords" style="width:150px;"autocomplete="on" maxlength="32"
                         data-toggle="tooltip" title="入力に該当した人員の候補を一覧に表示します。表示された人員を選択した場合、その番号が管理者人員番号に表示されます。">
                         <datalist id="keywords">
 @for($j = 0; $j < count($personnel_data);$j++)
@@ -175,11 +175,11 @@
                         data-toggle="tooltip" title="部署情報を修正、抹消できる管理者を変更する場合、ここを修正します 管理者自身とシステム管理者だけが修正できます"></p>
                     </div>
                     <div class="col-3" style="padding:0px">
-                        <p>管理者名：{{$click_management_lists[0]}}</p>
+                        <p>管理者名：<a href="{{ route('plbs01.show',[session('client_id'),$click_department_data[0]->management_personnel_id])}}">{{$click_management_lists[0]}}</a></p>
                     </div>
                     <div class="col" style="padding:0px">
                     <p>管理者検索：
-                        <input type="search" id="search-list" list="keywords" style="width:150px;" autocomplete="on"
+                        <input type="search" id="search-list" list="keywords" style="width:150px;" autocomplete="on" maxlength="32"
                         data-toggle="tooltip" title="入力に該当した人員の候補を一覧に表示します。表示された人員を選択した場合、その番号が管理者人員番号に表示されます。">
                         <datalist id="keywords">
 @for($j = 0; $j < count($personnel_data);$j++)
@@ -333,11 +333,11 @@
                         data-toggle="tooltip" title="部署情報を修正、抹消できる管理者を変更する場合、ここを修正します 管理者自身とシステム管理者だけが修正できます"></p>
                     </div>
                     <div class="col-3" style="padding:0px">
-                        <p>管理者名：{{$click_management_lists[0]}}</p>
+                        <p>管理者名：<a href="{{ route('plbs01.show',[session('client_id'),$click_personnel_data[0]->management_personnel_id])}}">{{$click_management_lists[0]}}</a></p>
                     </div>
                     <div class="col" style="padding:0px">
                     <p>管理者検索：
-                        <input type="search" id="search-list" list="keywords" style="width:150px;" autocomplete="on"
+                        <input type="search" id="search-list" list="keywords" style="width:150px;" autocomplete="on" maxlength="32"
                         
                         data-toggle="tooltip" title="入力に該当した人員の候補を一覧に表示します。表示された人員を選択した場合、その番号が管理者人員番号に表示されます。">
                         <datalist id="keywords">

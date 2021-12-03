e0e25e78640a4f4f0b6f2034e81a74cfb0232515
unknown <hioki@b-forme.net>
2021-12-03 13:37:55 +0900

    'マイグレーションの作り直し'

---
+++ work_management/database/migrations/2021_12_03_131037_create_dccmks_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDccmksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dccmks', function (Blueprint $table) {
+            $table->string('client_id','10');
+            $table->string('lower_id','10');
+            $table->string('high_id','10');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dccmks');
+    }
+}
---
+++ work_management/database/migrations/2021_12_03_132135_create_dccmta_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDccmtaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dccmta', function (Blueprint $table) {
+            $table->primary(['client_id', 'projection_id']);
+            $table->string('client_id','10');
+            $table->string('projection_id','10');
+            $table->string('projection_source_id','10');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dccmta');
+    }
+}

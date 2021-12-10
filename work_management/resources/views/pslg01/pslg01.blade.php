dff9a0715422949b12b909b6f957a01cdadab91f
kazunorimekw <k_maekawa@b-forme.net>
2021-12-10 15:39:36 +0900

    12/10更新

--- work_management/resources/views/pslg01/pslg01.blade.php
+++ work_management/resources/views/pslg01/pslg01.blade.php
@@ -14,20 +14,20 @@
 
                     <div class="row">
                         <p style="text-align: center">検索したい条件などを入力して「表示する」ボタンを押してください<br>
-                            【　* 部署人員番号が空白の場合、全ての人員名が表示されます。】</p>
+                            【* 部署人員番号が空白の場合、全ての人員名が表示されます。】</p>
                         <div class="col-4">
                             <form action="{{route('pslg01.create')}}" method="post" id="create">
                                 @csrf
                                 <!-- form id = "create"  下記のname属性を入れ子で対応　-->
                                 <!--   name = [ management_number, management_name, 
-                                                 startdate, finishdate, system_management
+                                                 startdate, finishdate, 
                                                  check[], search, "表示する"  ]  -->
                             </form>
-                            <p>顧客番号： <input type="text" name="name" style="width:100px;" placeholder="半角英数字で入力"></p>
-                            <p>顧客名　： <a href="#" style="color:black;" title="クリックにより顧客詳細に遷移します">　(例)前川一号生</a></p>
+                            <p>顧客番号&nbsp;&nbsp;: <input type="text" name="name" style="width:100px;" placeholder="半角英数字で入力"></p>
+                            <p>顧客名&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: <a href="#" style="color:black;" title="クリックにより顧客詳細に遷移します">(例)前川一号生</a></p>
 
-                            <p class="box" title="入力に該当した顧客の候補を一覧に表示します。表示された人員を選択した場合、その番号が顧客番号に表示されます">顧客検索：　
-                                <select class="box" style="width:100px;">
+                            <p class="box" title="入力に該当した顧客の候補を一覧に表示します。表示された人員を選択した場合、その番号が顧客番号に表示されます">顧客検索&nbsp;&nbsp;:
+                                <select class="box" style="width:100px; margin-left:5px;">
                                     <option selected name="client_id" value=""></option>
                                     @foreach($personnel_data as $parson)
                                     <option name="client_id" value="{{$parson->client_id}}"> {{$parson->client_id}}</option>
@@ -37,10 +37,10 @@
                         </div>
 
                         <div class="col-4">
-                            <p>部署人員番号：<input type="text" id="management_number" name="management_number" form="create" maxlength="10" data-toggle="tooltip" title="部署情報を修正、抹消できる管理者を変更する場合、ここを修正します 管理者自身とシステム管理者だけが修正できます" value="" style="width:100px;"></p>
-                            <p>部署人員名：<span id="management_name" name="management_name" style="text-decoration: underline" data-toggle="tooltip" title="クリックにより顧客詳細に遷移します"></span></p>
+                            <p>部署人員番号&nbsp;&nbsp;:&nbsp;&nbsp;<input type="text" id="management_number" name="management_number" form="create" maxlength="10" data-toggle="tooltip" title="部署情報を修正、抹消できる管理者を変更する場合、ここを修正します 管理者自身とシステム管理者だけが修正できます" value="" style="width:100px;"></p>
+                            <p>部署人員名&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:<span id="management_name" name="management_name" style="text-decoration:underline; margin-left: 10px;" data-toggle="tooltip" title="クリックにより顧客詳細に遷移します"></span></p>
 
-                            <p>部署人員検索：
+                            <p>部署人員検索&nbsp;&nbsp;:
                                 <input type="text" id="search-list" list="keywords" style="width:150px;" autocomplete="on" name="management_name" form="create" data-toggle="tooltip" title="入力に該当した人員の候補を一覧に表示します。表示された人員を選択した場合、その番号が管理者人員番号に表示されます。">
                                 <datalist id="keywords">
                                     @for($j = 0; $j < count($personnel_data);$j++) <option value="{{$personnel_data[$j]->name}}" label="{{$personnel_data[$j]->personnel_id}}" managment="{{$personnel_data[$j]->system_management}}">
@@ -53,20 +53,20 @@
                         </div>
 
                         <div class="col-4">
-                            <p>開始日時： <input type="datetime-local" name="startdate" value="{{date('Y-m-d', strtotime('today'))}}T00:00" form="create"></p>
-                            <p>終了日時： <input type="datetime-local" name="finishdate" value="{{date('Y-m-d\TH:i',strtotime('now'))}}" form="create"></p>
+                            <p>開始日時: <input type="datetime-local" name="startdate" value="{{date('Y-m-d', strtotime('today'))}}T00:00" form="create"></p>
+                            <p>終了日時: <input type="datetime-local" name="finishdate" value="{{date('Y-m-d\TH:i',strtotime('now'))}}" form="create"></p>
                         </div>
                     </div>
                     <div class="container">
                         <div class="row justify-content-start">
                             <div class="col-3">
-                                　<input type="checkbox" name="check[]" value="nm" title="通常メッセージログを表示するかどうかを指定します" form="create"> 通常メッセージ
+                                <input type="checkbox" name="check[]" value="nm" title="通常メッセージログを表示するかどうかを指定します" form="create"> 通常メッセージ
                             </div>
                             <div class="col-3">
-                                　<input type="checkbox" name="check[]" value="wn" title="警告メッセージログを表示するかどうかを指定します" form="create" checked> 警告メッセージ
+                                <input type="checkbox" name="check[]" value="wn" title="警告メッセージログを表示するかどうかを指定します" form="create" checked> 警告メッセージ
                             </div>
                             <div class="col-3">
-                                　<input type="checkbox" name="check[]" value="er" title="異常メッセージログを表示するかどうかを指定します" form="create" checked> 異常メッセージ
+                                <input type="checkbox" name="check[]" value="er" title="異常メッセージログを表示するかどうかを指定します" form="create" checked> 異常メッセージ
                             </div>
                             <div class="col-3">
                                 <input type="checkbox" name="check[]" value="ok" title="正常メッセージログを表示するかどうかを指定します" checked form="create"> 正常メッセージ
@@ -83,7 +83,7 @@
                         </div>
                     </div>
                     <div class="row mt-3">
-                        <div class="col-10">　文字検索：<input type="text" name="search" style="width:32rem;" placeholder="入力可能な文字数は３２  全角、半角英数字、一覧操作領域" form="create">
+                        <div class="col-10">文字検索&nbsp;&nbsp;:&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="search" style="width:32rem;" placeholder="入力可能な文字数は３２  全角、半角英数字、一覧操作領域" form="create">
                         </div>
                         <div class="col-2">
                             <input type="submit" value="表示する" form="create">
@@ -94,7 +94,7 @@
                         <div class="col-10"></div>
                         <div class="col-2">
                             @isset($count)
-                            件数　：{{$count}} 件
+                            件数:&nbsp;&nbsp;{{$count}} 件
                             @endif
                         </div>
                     </div>

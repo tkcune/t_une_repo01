5fbf70916e1af2d0fd47a4610dfe1f9f4b920aad
unknown <hioki@b-forme.net>
2022-01-07 13:50:40 +0900

    'セッションの削除命令を移動する'

--- work_management/app/Libraries/php/HeaderMessage.php
+++ work_management/app/Libraries/php/HeaderMessage.php
@@ -4,11 +4,9 @@
 
     class HeaderMessage {
 
-        public static function clear_header_message(){
-            session()->forget('message');
-        }
         public static function set_header_message(string $message_code, ...$message)
         {
+            session()->forget('message');
             $message = Message::get_message($message_code, $message);
             session(['message'=>$message[0]]);
         }

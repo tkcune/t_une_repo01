01b14405f6a13b6be0f165d9f50ffb95e9ede1d7
unknown <hioki@b-forme.net>
2022-01-07 14:50:59 +0900

    'レイアウトを整える'

--- work_management/resources/views/psnw01/psnw01.blade.php
+++ work_management/resources/views/psnw01/psnw01.blade.php
@@ -5,78 +5,74 @@
 {{-- フォーム画面表示--}}
 <div class="col border border-primary" style="padding:10px;">
 
-<form action="{{route('psnw01.create')}}" method="post">
+    <form method="post">
         @csrf
         
-        <div class="details-area border border-dark bg-warning" style="padding:10px;" id="parent">
-            <div class="row">
-                <div class="col-4">
-                    <p>メールアカウント名<input type="text" name="name" maxlength="256" data-toggle="tooltip" title="メールアカウント名を入力します"></p>
-                </div>
-                <div class="col-4">
-                    <p>メールアドレス<input type="text" name="email" maxlength="256" data-toggle="tooltip" title="メールアドレスを入力します"></p>
-                </div>
-            </div>
-
-            <div class="row" >
-                <div class="col-4">
-                    <p>パスワード：<input type="password" id="password" name="password" maxlength="256" data-toggle="tooltip" title="パスワードを入力してください。"></p>
-                </div>
-            </div>
-            <br>
-            <div class="row" >
-                <div class="col-3" style="padding:0px">
-                    <p>受信サーバ：<input type="text" id="recieving_server" name="recieving_server" maxlength="256" data-toggle="tooltip" title="受信サーバを入力してください。"></p>
-                </div>
-                <div class="col" style="padding:0px">
-                <p>受信サーバ通信方式</p>
-                </div>
-                <div class="col" style="padding:0px" data-toggle="tooltip" title="受信プロトコルを選択してください">
-                <select name = "recieving_server_way" id="receive_combobox">
-                    <option value="1" selected>POP</option>
-                    <option value="2">IMAP</option>
-                </select>
-                </div>
-                <div class="col" style="padding:0px">
-                    <p>受信サーバポート番号：<input type="text" id="recieving_port_number" name="recieving_port_number" maxlength="6" data-toggle="tooltip" title="受信サーバのポート番号を入力してください。" value="995"></p>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-3" style="padding:0px">
-                    <p>送信サーバ：<input type="text" id="sending_server" name="sending_server" maxlength="256" data-toggle="tooltip" title="送信サーバを入力してください。"></p>
-                </div>
-                <div class="col" style="padding:0px">
-                    <p>送信サーバ通信方式：SMTP</p>
-                </div>
-                <div class="col" style="padding:0px">
-                    <p>送信サーバポート番号：<input type="text" id="sending_port_number" name="sending_port_number" maxlength="6" data-toggle="tooltip" title="送信サーバのポート番号を入力してください。" value="587"></p>
-                </div>
-
-            </div>
+        <div class="border border-dark bg-warning" style="padding:10px;">
             <div class="row">
                 <div class="col">
-                    <input type="submit" value="確定" data-toggle="tooltip" title="クリックにより、登録、更新を確定します">
+                    メールアカウント名&nbsp;:&nbsp;<input type="text" name="name" maxlength="256" data-toggle="tooltip" title="メールアカウント名を入力します" value="sagyotest@b-forme.net">                </div>
+                <div class="col">
+                    メールアドレス&nbsp;:&nbsp;<input type="text" name="email" maxlength="256" data-toggle="tooltip" title="メールアドレスを入力します" value="sagyotest@b-forme.net">
+                </div>
+            </div>
+
+            <div style="padding: 10px 0;">
+                パスワード&nbsp;:&nbsp;<input type="password" id="password" name="password" maxlength="256" data-toggle="tooltip" title="パスワードを入力してください。" value="sagyopass">
+            </div>
+            <div class="row" style="padding: 15px 0;">
+                <div class="col">
+                    受信サーバ&nbsp;:&nbsp;<input type="text" id="recieving_server" name="recieving_server" maxlength="256" value = "pop3.muumuu-mail.com" data-toggle="tooltip" title="受信サーバを入力してください。">
+                </div>
+                <div class="col">
+                    受信サーバ通信方式&nbsp;:&nbsp;
+                    <select name = "recieving_server_way" id="receive_combobox"  data-toggle="tooltip" title="受信プロトコルを選択してください">
+                        <option value="1" selected>POP</option>
+                        <option value="2">IMAP</option>
+                    </select>
+                </div>
+            </div>
+            <div>
+                受信サーバポート番号&nbsp;:&nbsp;<input type="text" id="recieving_port_number" name="recieving_port_number" maxlength="6" data-toggle="tooltip" title="受信サーバのポート番号を入力してください。" value="995">
+            </div>
+            <div class="row"  style="padding: 15px 0;">
+                <div class="col">
+                    送信サーバ&nbsp;:&nbsp;<input type="text" id="sending_server" name="sending_server" maxlength="256" data-toggle="tooltip" title="送信サーバを入力してください。" value = "smtp.muumuu-mail.com">
+                </div>
+                <div class="col">
+                    送信サーバ通信方式&nbsp;:&nbsp;SMTP
+                </div>
+            </div>
+            <div>
+                送信サーバポート番号&nbsp;:&nbsp;<input type="text" id="sending_port_number" name="sending_port_number" maxlength="6" data-toggle="tooltip" title="送信サーバのポート番号を入力してください。" value="587">
+            </div>
+
+            <div class="row" style="padding: 10px 0;">
+                <div class="col-2">
+                    <input type="submit" value="確定" data-toggle="tooltip" title="クリックにより、登録、更新を確定します" formaction="{{ route('psnw01.create') }}">
+                </div>
+                <div class="col-2">
+                    <input type="submit" value="送信試験" data-toggle="tooltip" title="送信試験を行います" formaction="{{ route('psnw01.send') }}">
                 </div>
             </div>
     </form>
-
-    <form action="/" method="get">
-        @csrf
-        <input type="submit" value="送信試験" data-toggle="tooltip" title="送信試験を行います">
-    </form>
-    <form action="/" method="get">
-        @csrf
-        <input type="submit" value="受信試験" data-toggle="tooltip" title="受信試験を行います">
-    </form>
-    <div class="row">
-        <div>
+    <div class="row" style="padding: 10px 0;">
+        <div class="col-2">
+        <input type="submit" value="受信試験" data-toggle="tooltip" title="受信試験を行います" formaction="{{ route('psnw01.receive') }}">
+        </div>
+        <div class="col-2">
             受信メッセージ
         </div>
-        <div>
-            <textarea maxlength="512" style="width:800px; height: 100px;"></textarea>
+    </div>
+    <div class="border border-dark" style="padding: 10px 0;">
+        <div style="padding: 10px;">
+            @if(isset($mail))
+                @foreach($mail as $line)
+                    {!! $line !!}<br />
+                @endforeach
+            @endif
         </div>
     </div>
-    
 </div>
 
 @endsection

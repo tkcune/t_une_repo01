0b7e197a3c7974d310321eb396b564a8152fa5d1
yokoyamaryu1218 <97070988+yokoyamaryu1218@users.noreply.github.com>
2022-03-25 13:45:37 +0900

    Add files via upload

--- work_management/database/migrations/2022_03_03_140507_create_dcsb01_table.php
+++ work_management/database/migrations/2022_03_03_140507_create_dcsb01_table.php
@@ -11,33 +11,34 @@ class CreateDcsb01Table extends Migration
      * Run the migrations.
      * 作業場所のテーブル
      *
-     * @param $space_id 作業場所ID
-     * @param $space_name　作業場所名称
-     * @param $management_personnel_id　管理者人員番号
-     * @param $post_code 郵便番号
-     * @param $prefectural_office_location 都道府県
-     * @param $address 市区町村
-     * @param $URL アドレス
-     * @param $remarks 備考
-     * @param $created_at 登録日
-     * @param $updated_at 修正日
-     *
      * @return void
      */
     public function up()
     {
         Schema::create('dcsb01', function (Blueprint $table) {
-            $table->primary(['client_id', 'space_id']);            $table->string('client_id', '10');
-            $table->string('space_id', '10');
-            $table->string('space_name', '32');
-            $table->string('management_personnel_id', '10');
-            $table->unsignedInteger('post_code')->index();
-            $table->string('prefectural_office_location');
-            $table->string('address');
-            $table->string('URL', '256')->nullable();
-            $table->string('remarks', '512')->nullable();
-            $table->timestamp('created_at')->useCurrent();
-            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+            $table->primary(['client_id', 'space_id']);
+            $table->string('client_id', '10')
+                ->comment('顧客ID');
+            $table->string('space_id', '10')
+                ->comment('作業場所ID');
+            $table->string('space_name', '32')
+                ->comment('作業場所名称');
+            $table->string('management_personnel_id', '10')
+                ->comment('管理者人員番号');
+            $table->unsignedInteger('post_code')->index()
+                ->comment('郵便番号');
+            $table->string('prefectural_office_location')
+                ->comment('都道府県');
+            $table->string('address')
+                ->comment('市区町村');
+            $table->string('URL', '256')->nullable()
+                ->comment('地図URL');
+            $table->string('remarks', '512')->nullable()
+                ->comment('備考');
+            $table->timestamp('created_at')->useCurrent()
+                ->comment('登録日');
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'))
+                ->comment('修正日');
         });
     }
 

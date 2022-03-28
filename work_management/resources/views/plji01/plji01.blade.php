5ce863590968698d91a8f8b49691c37c6165d0f2
unknown <hioki@b-forme.net>
2022-03-28 13:08:25 +0900

    'レスポンシブデザイン横画面固定(仮保存)'

--- work_management/resources/views/plji01/plji01.blade.php
+++ work_management/resources/views/plji01/plji01.blade.php
@@ -1,30 +1,29 @@
-<div class="row margin-reset">
-    <div class="col">
-        <div>
-            <table id="ji-table" class="ji-table table_sticky table table-striped" style="margin: 0 0">
+
+        <div class="border border-dark" style="max-height: 100px; overflow-y:scroll;">
+            <table id="ji-table" class="tablesorter hasStickyHeaders" style="margin: 0 0">
                 <thead>
                     <tr>
-                        <th style="white-space: nowrap; font-size: 8px;">人員番号</th>
-                        <th style="white-space: nowrap; font-size: 8px;">氏名</th>
-                        <th style="white-space: nowrap; font-size: 8px;">所属部署</th>
-                        <th style="white-space: nowrap; font-size: 8px;">状態</th>
-                        <th style="white-space: nowrap; font-size: 8px;">ID</th>
-                        <th style="white-space: nowrap; font-size: 8px;">PW更新</th>
-                        <th style="white-space: nowrap; font-size: 8px;">権限</th>
-                        <th style="white-space: nowrap; font-size: 8px;">操作</th>
+                        <th>人員番号</th>
+                        <th>氏名</th>
+                        <th>所属部署</th>
+                        <th>状態</th>
+                        <th>ID</th>
+                        <th>PW更新</th>
+                        <th>権限</th>
+                        <th>操作</th>
                     </tr>
                 </thead>
                 <tbody>
                 @foreach($names as $name)
                     <tr>
-                        <td style="font-size: 8px;">{{ $name->personnel_id}}</td>
+                        <td>{{ $name->personnel_id}}</td>
                         @if($name->operation_start_date > \Carbon\Carbon::today()->format('Y-m-d') || (!(null == $name->operation_end_date) && \Carbon\Carbon::today()->format('Y-m-d') > $name->operation_end_date))
-                        <td style="font-size: 8px;"><s><a href="{{ route('plbs01.show',[session('client_id'),$name->personnel_id])}}" data-toggle="tooltip" title="運用中ではありません">{{$name->name}}</a> </s> </td>
+                        <td><s><a href="{{ route('plbs01.show',[session('client_id'),$name->personnel_id])}}" data-toggle="tooltip" title="運用中ではありません">{{$name->name}}</a> </s> </td>
                         @else
-                        <td style="font-size: 8px;"><a href="{{ route('plbs01.show',[session('client_id'),$name->personnel_id])}}" data-toggle="tooltip" title="クリックにより、当該人員に遷移します">{{$name->name}}</a></td>
+                        <td><a href="{{ route('plbs01.show',[session('client_id'),$name->personnel_id])}}" data-toggle="tooltip" title="クリックにより、当該人員に遷移します">{{$name->name}}</a></td>
                         @endif 
-                        <td style="font-size: 8px;"><a href="{{ route('plbs01.show',[session('client_id'),$personnel_high[$loop->index]->department_id])}}" data-toggle="tooltip" title="クリックにより、所属部署に遷移します">{{$personnel_high[$loop->index]->name}}</a>
-                        <td style="font-size: 8px;">
+                        <td><a href="{{ route('plbs01.show',[session('client_id'),$personnel_high[$loop->index]->department_id])}}" data-toggle="tooltip" title="クリックにより、所属部署に遷移します">{{$personnel_high[$loop->index]->name}}</a>
+                        <td>
                         @switch($name->status)
                             @case(10)
                                 応募
@@ -46,10 +45,10 @@
                                 @break
                         @endswitch
                         </td>
-                        <td style="font-size: 8px;">aaa02</td>
-                        <td style="font-size: 8px;">{{$name->password_update_day}}</td>
-                        <td style="font-size: 8px;">---------</td>
-                        <td style="font-size: 8px;">【<a href="{{ route('pa0001.clipboard',$name->personnel_id)}}">複写</a>】
+                        <td>aaa02</td>
+                        <td>{{$name->password_update_day}}</td>
+                        <td>---------</td>
+                        <td>【<a href="{{ route('pa0001.clipboard',$name->personnel_id)}}">複写</a>】
                             【<p id="list_delete{{$loop->index}}" name="bs_delete" style="pointer-events: none; display:inline-block; text-decoration:underline; margin:0px;" onclick="event.preventDefault(); document.getElementById('delete{{$loop->index}}').submit();">削除</p>】
                             <form id="delete{{$loop->index}}" action="{{ route('psji01.destroy',[session('client_id'),$name->personnel_id])}}" method="post" style="display: none;">
                                 @csrf
@@ -60,5 +59,3 @@
                 </tbody>
             </table>
         </div>
-    </div>
-</div>
--- work_management/resources/views/pscm01/pscm10.blade.php
+++ work_management/resources/views/pscm01/pscm10.blade.php
@@ -1,21 +1,15 @@
 <div class="row margin-reset">
-    <div style="padding:0px">
+    <div class="col-4">
+        <p>管理者番号：<input type="text" id="management_number" name="management_number" maxlength="10" value="{{$click_personnel_data->management_personnel_id}}" style="width:100px;"
+        data-toggle="tooltip" title="部署情報を修正、抹消できる管理者を変更する場合、ここを修正します 管理者自身とシステム管理者だけが修正できます"></p>
+    </div>
+    <div class="col-3" style="padding:0px">
         <p>管理者名：<a href="{{ route('plbs01.show',[session('client_id'),$click_personnel_data->management_personnel_id])}}">{{$click_management_lists[0]}}</a></p>
     </div>
-    <div class="row">
-        <div class="col">
-            <div>管理者番号：</div>
-            <div>
-                <input type="text" id="management_number" name="management_number" maxlength="10" value="{{$click_personnel_data->management_personnel_id}}" style="width:100px;"
-                data-toggle="tooltip" title="部署情報を修正、抹消できる管理者を変更する場合、ここを修正します 管理者自身とシステム管理者だけが修正できます"></p>
-            </div>
-        </div>
-        <div class="col" style="padding:0px">
-            <div>管理者検索：</div>
-            <div>
-                <input type="search" id="search-list" list="keywords" style="width:150px;" autocomplete="on" maxlength="32"
-                        
-                data-toggle="tooltip" title="入力に該当した人員の候補を一覧に表示します。表示された人員を選択した場合、その番号が管理者人員番号に表示されます。">
+    <div class="col" style="padding:0px">
+        <p>管理者検索：
+            <input type="search" id="search-list" list="keywords" style="width:150px;" autocomplete="on" maxlength="32"        
+            data-toggle="tooltip" title="入力に該当した人員の候補を一覧に表示します。表示された人員を選択した場合、その番号が管理者人員番号に表示されます。">
                 <datalist id="keywords">
                         
 @for($j = 0; $j < count($all_personnel_data);$j++)
@@ -24,7 +18,6 @@
 @endif
 @endfor
                 </datalist>
-            </div>
-        </div>
+        </p>
     </div>
 </div>
